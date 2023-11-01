af23a27268e09eb37e7410f2cd3790b74e5afe29
harshit <harshit@zeddlabz.com>
2023-11-01 19:05:13 +0530

    Done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const session = require("express-session");
+// const session = require("express-session");
 const app = express();
 
 require("dotenv").config();
@@ -15,45 +15,56 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "session",
-    cookie: {
-      // maxAge: 30 * 60 * 60 * 1000,
-      sameSite: "lax",
-      // secure: true,
-    },
-  })
-);
+// app.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: "session",
+//     cookie: {
+//       // maxAge: 30 * 60 * 60 * 1000,
+//       sameSite: "lax",
+//       // secure: true,
+//     },
+//   })
+// );
+
+// app.post("/set", async (req, res) => {
+//   try {
+//     req.session.jwt = "hello";
+//     res.status(200).send({ message: "Cookie Saves Successfully " });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+
 
 app.post("/set", async (req, res) => {
   try {
-    req.session.jwt = "hello";
-    res.status(200).send({ message: "Cookie Saves Successfully " });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.post("/set1", async (req, res) => {
-  try {
-    res.cookie("jwt", "updated", {
-      // httpOnly: true,
-      sameSite: "none",
-      secure: true,
-    });
+    res.cookie("jwt", accessToken, { httpOnly: true, sameSite: 'strict', secure: true });
     res.status(200).send({ message: "Cookie Set Successfully" });
   } catch (error) {
     console.log(error);
   }
 });
 
+// app.get("/get", async (req, res) => {
+//   try {
+//     console.log(req.session.jwt);
+//     if (req.session.jwt) {
+//       res.status(200).send({ message: req.session.jwt });
+//     } else {
+//       res.status(200).send({ message: "Cookie not present"});
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
 app.get("/get", async (req, res) => {
   try {
-    console.log(req.session.jwt);
-    if (req.session.jwt) {
+    console.log(req.cookies["jwt"]);
+    if (req.cookies["jwt"]) {
       res.status(200).send({ message: req.session.jwt });
     } else {
       res.status(200).send({ message: "Cookie not present"});
@@ -63,8 +74,14 @@ app.get("/get", async (req, res) => {
   }
 });
 
+// app.post("/remove", (req, res) => {
+//   req.session.jwt = null;
+
+//   res.send({ message: "Logged out." });
+// });
+
 app.post("/remove", (req, res) => {
-  req.session.jwt = null;
+  res.clearCookie("jwt", { path: "/" });
 
   res.send({ message: "Logged out." });
 });
