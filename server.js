3707cb96d2ff43819003d5627c0e88469d27dba8
harshit <harshit@zeddlabz.com>
2023-11-06 12:58:45 +0530

    Done

--- server.js
+++ server.js
@@ -41,7 +41,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/set", async (req, res) => {
   try {
-    res.cookie("jwt", "hello", { httpOnly: true, sameSite: 'strict', secure: true });
+    res.cookie("jwt", "hello", { 
+      // httpOnly: true, 
+      sameSite: 'strict',
+      secure: true });
     res.status(200).send({ message: "Cookie Set Successfully" });
   } catch (error) {
     console.log(error);
